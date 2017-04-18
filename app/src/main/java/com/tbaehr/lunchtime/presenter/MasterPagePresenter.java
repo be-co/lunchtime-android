9d56948a68e30141da072e184aaffda2519ee4fa
Timo Bähr <timo.baehr@gmail.com>
2017-04-18 22:11:42 +0200

    Removed the feature to enter fix places inside search view

--- app/src/main/java/com/tbaehr/lunchtime/presenter/MasterPagePresenter.java
+++ app/src/main/java/com/tbaehr/lunchtime/presenter/MasterPagePresenter.java
@@ -684,7 +684,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.design.widget.NavigationView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -801,8 +800,6 @@ public class MasterPagePresenter extends CustomBasePresenter<IMasterPageViewCont
         IMasterPageViewContainer.MaterialSearchViewListener listener = new IMasterPageViewContainer.MaterialSearchViewListener() {
             SuggestionItem[] suggestionItems = getSuggestionItems();
 
-            AsyncTask mTask;
-
             @Override
             public boolean onQueryTextSubmit(final String query) {
                 final IMasterPageViewContainer view = getView();
@@ -877,60 +874,11 @@ public class MasterPagePresenter extends CustomBasePresenter<IMasterPageViewCont
 
             @Override
             public void onQueryTextChange(final String newText) {
-                if (mTask != null) {
-                    mTask.cancel(true);
-                }
-                mTask = new AsyncTask<Void, Void, SuggestionItem[]>() {
-                    @Override
-                    protected SuggestionItem[] doInBackground(Void... params) {
-                        try {
-                            if (newText.isEmpty() || newText.equals(suggestionItems[0].getText())) {
-                                return suggestionItems;
-                            }
-
-                            List<Address> foundPlaces = LocationHelper.getAddressFromPlace(newText);
-                            SuggestionItem[] suggestions = new SuggestionItem[suggestionItems.length + foundPlaces.size()];
-                            suggestions[0] = suggestionItems[0];
-                            int i;
-                            if (foundPlaces.size() == 0) {
-                                return suggestionItems;
-                            }
-                            for (i = 0; i < foundPlaces.size(); i++) {
-                                suggestions[i+1] = new SuggestionItem(false, R.drawable.ic_location_grey, addressToString(foundPlaces.get(i)));
-                            }
-                            i++;
-                            for (int j = 1; j < suggestionItems.length; j++) {
-                                SuggestionItem suggestionItem = suggestionItems[j];
-                                suggestions[i] = suggestionItem;
-                                i++;
-                            }
-
-                            StringBuilder sb = new StringBuilder("[");
-                            for (SuggestionItem s : suggestions) {
-                                sb.append(s.getText()).append(", ");
-                            }
-                            sb.append("]");
-                            Log.v("TimTim", sb.toString());
-
-                            return suggestions;
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        return suggestionItems;
-                    }
-
-                    @Override
-                    protected void onPostExecute(SuggestionItem[] suggestionItems) {
-                        super.onPostExecute(suggestionItems);
-                        mTask = null;
-                        if (suggestionItems != null) {
-                            view.setSearchViewSuggestions(suggestionItems);
-                        }
-                    }
-                }.execute();
+                // ;
             }
         };
         view.inflateSearchView(listener);
+        view.setSearchViewSuggestions(getSuggestionItems());
     }
 
     private String addressToString(Address address) {
